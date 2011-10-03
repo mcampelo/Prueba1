428d656d72059da595a269d39f6014883768a52b
mcampelo <mcampelo@gmail.com>
2011-10-03 14:49:10 -0300

    creado archivo creado_local.php desde netbeans

---
+++ creado_local.php
@@ -0,0 +1,16 @@
+<?php
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        phpinfo();
+        ?>
+    </body>
+</html>
+
+?>
